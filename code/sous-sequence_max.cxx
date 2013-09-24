a560d89d79bc1aea4c68a11b03490aa091cdffa0
Eloi Perdereau <eloi@perdereau.eu>
2013-09-24 18:38:25 +0200

    put everything in one file
        test_asymptote() is the former test_ssq_max.cxx's main
        main_algo() is the former sous-sequence_max.cxx's main

--- code/sous-sequence_max.cxx
+++ code/sous-sequence_max.cxx
@@ -112,7 +112,33 @@ namespace algo {
 
 }
 
-int main (int argc, char * argv []) {
+int test_asymptote() {
+    unsigned n_max = 50000;
+    unsigned step = 1000;
+    clock_t start, end;
+
+    cout << "n;secondes" << endl;
+    for (unsigned i(1); i <= n_max/step; ++i) {
+        unsigned n = i * step;
+        int T[n];
+
+        // generate random array of n integers from -99 to 99
+        for (unsigned j(0); j < n; ++j) {
+            int sign = rand() % 2;
+            int x = rand() % 100;
+            T[j] = sign ? -x : x;
+        }
+
+        cout << n << ";";
+        start = clock();
+        algo::algo_incr(T, n);
+        end = clock();
+        cout << (end - start) / float(CLOCKS_PER_SEC) << endl;
+    }
+    return 0;
+}
+
+int main_algo(int argc, char * argv []) {
 
     if (argc < 3) {
         cout << "Usage : <nb_elem> <elem_1> ... <elem_n>" << endl;
@@ -141,4 +167,11 @@ int main (int argc, char * argv []) {
     cout << res << endl;
 
     return 0;
+}
+
+int main (int argc, char * argv []) {
+    int r;
+    /* * / r = main_algo(argc, argv); /* */
+    /* */ r = test_asymptote();      /* */
+    return r;
 }
